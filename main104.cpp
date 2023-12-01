8ae33858166e0065957308c51c7887ae97d3a1eb
atamofidi <151543259+atamofidi@users.noreply.github.com>
2023-12-01 12:17:39 -0800

    Add files via upload

---
+++ main104.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+ main() {
+
+    int season;
+    
+    cout<< "pleasa enter season number : ";
+    cin>>season;
+    
+    switch (season){ 
+
+    	case 1:
+		
+    		cout<< "spring";
+    		break;
+    	case 2:
+    		cout<< "summer";
+    		break;
+		case 3:
+		    cout<<	"fall" ;
+		    break;
+		case 4: 
+		    cout<<  "winter";
+			break;   		
+	}
+
+
+}
